a054508e800bff827141b8cd5eb7f66a010bd341
tejaswi-6189 <tejaswi6089@gmail.com>
2026-06-21 16:13:57 +0530

    Add files via upload

---
+++ Week-1/Exercise-2-FactoryMethodPattern/FactoryMethodPatternExample.java
@@ -0,0 +1,60 @@
+interface Document {
+    void open();
+}
+
+class WordDocument implements Document {
+    public void open() {
+        System.out.println("Opening Word Document...");
+    }
+}
+
+class PdfDocument implements Document {
+    public void open() {
+        System.out.println("Opening PDF Document...");
+    }
+}
+
+class ExcelDocument implements Document {
+    public void open() {
+        System.out.println("Opening Excel Document...");
+    }
+}
+
+abstract class DocumentFactory {
+    abstract Document createDocument();
+}
+
+class WordFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new WordDocument();
+    }
+}
+
+class PdfFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new PdfDocument();
+    }
+}
+
+class ExcelFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new ExcelDocument();
+    }
+}
+
+public class FactoryMethodPatternExample {
+    public static void main(String[] args) {
+
+        DocumentFactory wordFactory = new WordFactory();
+        Document wordDoc = wordFactory.createDocument();
+        wordDoc.open();
+
+        DocumentFactory pdfFactory = new PdfFactory();
+        Document pdfDoc = pdfFactory.createDocument();
+        pdfDoc.open();
+
+        DocumentFactory excelFactory = new ExcelFactory();
+        Document excelDoc = excelFactory.createDocument();
+        excelDoc.open();
+    }
+}
