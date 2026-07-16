115e520e71e7c98a1c270493ac42c4bc842c77b1
tejaswi-6189 <tejaswi6089@gmail.com>
2026-07-16 14:46:32 +0530

    Add files via upload

---
+++ Week-4/Microservices/account/src/main/java/com/cognizant/account/model/Account.java
@@ -0,0 +1,41 @@
+package com.cognizant.account.model;
+
+public class Account {
+
+    private String accountNumber;
+    private String accountType;
+    private double balance;
+
+    public Account() {
+    }
+
+    public Account(String accountNumber, String accountType, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
