57cd14a74e39b2f9d5e8cddcf8bc3ee61db7ad4b
tejaswi-6189 <tejaswi6089@gmail.com>
2026-07-16 14:43:07 +0530

    Add files via upload

---
+++ Week-4/Microservices/WWeek4/loan/src/main/java/com/cognizant/loan/controller/LoanController.java
@@ -0,0 +1,21 @@
+package com.cognizant.loan.controller;
+
+import com.cognizant.loan.model.Loan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoanController {
+
+    @GetMapping("/loans/{number}")
+    public Loan getLoan(@PathVariable String number) {
+
+        return new Loan(
+                number,
+                "Home Loan",
+                2500000
+        );
+
+    }
+}
